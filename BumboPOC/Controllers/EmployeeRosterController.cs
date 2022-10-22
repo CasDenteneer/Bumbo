01fd4e14ea42da85031595252f4f458ace9e7fa2
Freek <103433440+FreekStraten@users.noreply.github.com>
2022-10-22 15:50:56 +0200

    Roster Employee view changes

---
+++ BumboPOC/Controllers/EmployeeRosterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BumboPOC.Controllers
+{
+    public class EmployeeRosterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
