1271bbc8719afaf417e9595316117889420e33be
Freek <103433440+FreekStraten@users.noreply.github.com>
2022-10-23 17:12:57 +0200

    Get data from database

--- BumboPOC/Controllers/EmployeeRosterController.cs
+++ BumboPOC/Controllers/EmployeeRosterController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BumboPOC.Models.DomainModels;
+using BumboPOC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BumboPOC.Controllers
 {
     public class EmployeeRosterController : Controller
     {
+        private readonly MyContext _MyContext;
+        public EmployeeRosterController(MyContext myContext)
+        {
+            _MyContext = myContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var _employee = _MyContext.Employees.Where(e => e.Id == 1).FirstOrDefault();
+            var _plannedShifts = _MyContext.PlannedShift.Where(s => s.EmployeeId == _employee.Id).ToList();
+
+            var _employeeShiftsViewModel = new EmployeeShiftsViewModel();
+            _employeeShiftsViewModel.Employee = _employee;
+            _employeeShiftsViewModel.PlannedShifts = _plannedShifts;
+
+            return View(_employeeShiftsViewModel);
         }
+
     }
+
 }
